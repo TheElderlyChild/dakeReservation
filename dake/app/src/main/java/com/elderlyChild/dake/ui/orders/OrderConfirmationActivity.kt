21e761fb0039c89d28509a735de14b989b744eb8
Wyse Ebbah <66836364+TheElderlyChild@users.noreply.github.com>
2021-03-08 18:07:06 -0500

    Create base file structure for order confirmation

--- dake/app/src/main/java/com/elderlyChild/dake/ui/orders/OrderConfirmationActivity.kt
+++ dake/app/src/main/java/com/elderlyChild/dake/ui/orders/OrderConfirmationActivity.kt
@@ -5,8 +5,24 @@ import android.os.Bundle
 import com.elderlyChild.dake.R
 
 class OrderConfirmationActivity : AppCompatActivity() {
+    
+    val order = getIntent().getExtra("order") 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_confirmation)
+        
+        initWidgets()
+        initButtons()
+        displayInfo()
+    }
+    
+    private fun initWidgets(){
+    }
+    
+    private fun displayInfo(){
+    }
+    
+    private fun initButtons(){
     }
 }
